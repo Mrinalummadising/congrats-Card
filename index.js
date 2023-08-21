45708e99d28d118eada1e4f67032e77f322cab50
Mrinalummadising <mrinalummadising.com>
2023-08-21 05:51:02 +0000

    congratsCard-App

---
+++ index.js
@@ -0,0 +1,15 @@
+const element = (
+  <div className="container">
+    <div>
+      <h1 className="heading">Congratulations</h1>
+    </div>
+    <div className="congrats-card">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1>Kiran V</h1>
+      <p>Vishnu Institute of Computer Education and Technology, Bhimavaram</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
